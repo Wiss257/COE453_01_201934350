94b6d0a35d3b3188c2d4a4d44314ae217dc1c032
Wiss257 <160674622+Wiss257@users.noreply.github.com>
2024-03-27 22:35:21 +0300

    HW3_Task1

---
+++ HW3/add.js
@@ -0,0 +1,20 @@
+const functions = require('@google-cloud/functions-framework');
+
+functions.http('add', (req, res) => {
+  if (req.method === 'POST') {
+    const { X, Y } = req.body;
+
+    const result = parseFloat(X) + parseFloat(Y);
+
+    const response = {
+      X: X,
+      Y: Y,
+      result: result
+    };
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(JSON.stringify(response));
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
+});
